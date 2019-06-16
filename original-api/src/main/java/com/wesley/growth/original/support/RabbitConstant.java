44a1d96c122b6bca96adfc74de97bafdef59528c
wesley <wangp_yong@163.com>
2019-06-16 15:24:52 +0800

    初始化

---
+++ original-api/src/main/java/com/wesley/growth/original/support/RabbitConstant.java
@@ -0,0 +1,28 @@
+package com.wesley.growth.original.support;
+
+/**
+ * @author Created by Wesley on 2018/7/23.
+ */
+public abstract class RabbitConstant {
+
+    private RabbitConstant() {
+
+    }
+
+    public static final String HOST = "192.168.101.109";
+
+    public static final int PORT = 5672;
+
+    public static final String VIRTUAL_HOST = "/";
+
+    public static final String USERNAME = "guest";
+
+    public static final String PASSWORD = "guest";
+
+    public static final String EXCHANGE_DIRECT_ORDER = "wesley.order";
+
+    public static final String ROUTING_KEY_ADD = "add";
+
+    public static final String QUEUE_ORDER_ADD = "wesley.order.add";
+
+}
